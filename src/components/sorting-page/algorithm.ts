34f62fa21103572f1583c04b16f46a704f1a4152
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-04-24 13:31:51 +0300

    переделать сортировку выбором  и анимацию

--- src/components/sorting-page/algorithm.ts
+++ src/components/sorting-page/algorithm.ts
@@ -1,38 +1,49 @@
+import { ElementStates } from "../../types/element-states";
+
+export interface ISort {
+  value: number;
+  state: ElementStates;
+};
+
+export const sort = (array: Array<ISort>, firstIndex: number, secondIndex: number) => {
+  return ([array[firstIndex], array[secondIndex]] = [array[secondIndex], array[firstIndex]]);
+};
+
 export const randomArr = () => {
   const size = Math.floor(Math.random() * (17 - 3)) + 3;
-  const array = [];
+  const array: Array<ISort> = [];
 
   for (let i = 0; i < size; i++) {
     const randomNumber = Math.floor(Math.random() * (100 - 0)) + 0;
-    array.push(randomNumber);
+    array.push({ value: randomNumber, state: ElementStates.Default });
   }
 
   return array;
 };
 
-export const selectionSort = (array: Array<number>) => {
-  const length = array.length;
-  for (let i = 0; i < length - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      [array[i], array[minIndex]] = [array[minIndex], array[i]];
-    }
-  }
-  return array;
-};
+// export const selectionSort = (array: Array<number>) => {
+// const length = array.length;
+// for (let i = 0; i < length - 1; i++) {
+//   let minIndex = i;
+//   for (let j = i + 1; j < length; j++) {
+//     if (array[j] < array[minIndex]) {
+//       minIndex = j;
+//     }
+//   }
+//   if (minIndex !== i) {
+//     [array[i], array[minIndex]] = [array[minIndex], array[i]];
+//   }
+// }
+// return array;
+// };
 
-export const bubbleSort = (array: Array<number>) => {
-  const length = array.length;
-  for (let i = 0; i < length - 1; i++) {
-    for (let j = 0; j < length - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        [array[j], array[j + 1]] = [array[j + 1], array[j]];
-      }
-    }
-  }
-};
+// export const bubbleSort = (array: Array<number>) => {
+//   const length = array.length;
+//   for (let i = 0; i < length - 1; i++) {
+//     for (let j = 0; j < length - i - 1; j++) {
+//       if (array[j] > array[j + 1]) {
+//         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+//       }
+//     }
+//   }
+// };
--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -4,19 +4,24 @@ import { RadioInput } from "../ui/radio-input/radio-input";
 import { Button } from "../ui/button/button";
 import s from './sorting-page.module.css';
 import { Direction } from "../../types/direction";
-import { randomArr, selectionSort, bubbleSort } from "./algorithm";
+import { randomArr, ISort, sort } from "./algorithm";
 import { Column } from "../ui/column/column";
+import { ElementStates } from "../../types/element-states";
+import { setDelay } from "../../constants/setDelay";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 
 
 export const SortingPage: React.FC = () => {
-  const [array, setArray] = useState<Array<number>>([]);
+  const [array, setArray] = useState<Array<ISort>>([]);
   const [radio, setRadio] = useState('selectionSort');
   const [loader, setLoader] = useState(false);
 
   const generateArray = (e: FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const newArr = randomArr();
-    setArray(newArr);
+
+    setArray(randomArr);
+    console.log(newArr)
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -24,33 +29,55 @@ export const SortingPage: React.FC = () => {
   };
 
   const handleBubbleSort = (array: Array<number>, direction: Direction) => {
-    bubbleSort(array);
     setLoader(true);
-    if (direction === Direction.Ascending) {
-      setArray(array);
-    } else if (direction === Direction.Descending) {
-      setArray(array.reverse())
+    const length = array.length;
+    for (let i = 0; i < length - 1; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        }
+      }
     }
     setLoader(false);
-  };
+  }; // доделать
 
-  const handleSelectionSort = (array: Array<number>, direction: Direction) => {
-    selectionSort(array);
+  const selectionSort = async (array: Array<ISort>, direction: Direction) => {
     setLoader(true);
-    if (direction === Direction.Ascending) {
-      setArray(array);
-    } else if (direction === Direction.Descending) {
-      setArray(array.reverse())
+    const length = array.length;
+    for (let i = 0; i <= length - 1; i++) {
+      let minIndex = i;
+      array[minIndex].state = ElementStates.Changing;
+      for (let j = i + 1; j < length; j++) {
+        array[j].state = ElementStates.Changing;
+        setArray([...array]);
+
+        await setDelay(SHORT_DELAY_IN_MS);
+
+        if (direction === Direction.Descending ? array[j].value > array[minIndex].value : array[j].value < array[minIndex].value) {
+          minIndex = j;
+          array[j].state = ElementStates.Changing;
+          array[minIndex].state = i === minIndex ? ElementStates.Changing : ElementStates.Default;
+        }
+        if (minIndex !== i) {
+          array[j].state = ElementStates.Default;
+        }
+        setArray([...array]);
+      }
+      sort(array, minIndex, i);
+      array[minIndex].state = ElementStates.Default;
+      array[i].state = ElementStates.Modified;
+      setArray([...array]);
     }
     setLoader(false);
   };
 
   const handleSort = (direction: Direction) => {
     if (radio === 'selectionSort') {
-      handleSelectionSort([...array], direction);
-    } else if (radio === 'bubbleSort') {
-      handleBubbleSort([...array], direction);
-    }
+      selectionSort(array, direction);
+  } 
+    // else if (radio === 'bubbleSort') {
+    //   handleBubbleSort(array, direction);
+    // }
   };
 
   return (
@@ -71,25 +98,26 @@ export const SortingPage: React.FC = () => {
           <Button
             text="По убыванию"
             extraClass={s.button}
-            sorting={Direction.Descending} 
+            sorting={Direction.Descending}
             onClick={() => handleSort(Direction.Descending)}
             isLoader={loader}
-            />
+          />
         </fieldset>
         <Button
           text="Новый массив"
           type="submit"
           onClick={generateArray}
-          extraClass={s.button} 
+          extraClass={s.button}
           isLoader={loader}
-          />
+        />
       </form>
       <ul className={s.ul}>
         {array.map((item, index) => {
           return (
-            <Column index={item} />
-          )
-        })
+            <li key={index}>
+              <Column index={item.value} state={item.state} />
+            </li>
+          )})
         }
       </ul>
     </SolutionLayout>
