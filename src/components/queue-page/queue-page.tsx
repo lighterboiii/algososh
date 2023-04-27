5979afa892bb6af8edc461206ebcbdb81b820c43
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-04-27 14:11:12 +0300

    реализовать удаление, добавление элементов очереди

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -4,13 +4,17 @@ import s from './queue.module.css';
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
-import { generateArray } from "./utils";
 import { q } from "./queue";
+import { ElementStates } from "../../types/element-states";
+import { setDelay } from "../../constants/setDelay";
+import { SHORT_DELAY_IN_MS } from "../../constants/delays";
+import { HEAD, TAIL } from "../../constants/element-captions";
 
 export const QueuePage: React.FC = () => {
 
   const [inputValue, setInputValue] = useState('');
   const [queue, setQueue] = useState<Array<string>>([]);
+  const [currIndex, setCurrIndex] = useState(-1);
   console.log(queue);
   const [loader, setLoader] = useState({
     add: false,
@@ -26,13 +30,47 @@ export const QueuePage: React.FC = () => {
     setInputValue(e.target.value);
   };
 
+  const addItem = async () => {
+    setLoader({ ...loader, add: true });
+    setCurrIndex(q.getTail());
+    await setDelay(SHORT_DELAY_IN_MS);
+    q.enqueue(inputValue);
+    setQueue([...q.toArray()]);
+    setCurrIndex(-1);
+    setInputValue('');
+    setLoader({ ...loader, add: false });
+  };
+
+  const delItem = async () => {
+    setLoader({ ...loader, delete: true });
+    setCurrIndex(q.getHead());
+    await setDelay(SHORT_DELAY_IN_MS);
+    q.dequeue();
+    setQueue([...q.toArray()]);
+    setCurrIndex(-1);
+    setLoader({ ...loader, delete: false });
+  };
+
+  const isHead = (index: number) => {
+    return index === q.getHead() && !q.isEmpty() ? HEAD : '';
+  };
+
+  const isTail = (index: number) => {
+    return index === q.getTail() && !q.isEmpty() ? TAIL : '';
+  };
+
   return (
     <SolutionLayout title="Очередь">
-      <form className={s.form}>
+      <form className={s.form} onSubmit={e => e.preventDefault} >
         <fieldset className={s.fieldset} >
-          <Input extraClass={s.input} value={inputValue} onChange={handleChange} />
-          <Button text="Добавить" />
-          <Button text="Удалить" />
+          <Input
+            extraClass={s.input}
+            value={inputValue}
+            onChange={handleChange}
+            maxLength={4}
+            isLimitText={true} />
+          <Button text="Добавить" isLoader={loader.add} disabled={loader.delete || !inputValue} onClick={addItem} />
+          <Button text="Удалить" isLoader={loader.delete} disabled={loader.add || q.isEmpty()} onClick={delItem} />
         </fieldset>
         <Button text="Очистить" extraClass={s.formButton} />
       </form>
@@ -40,7 +78,17 @@ export const QueuePage: React.FC = () => {
         {queue.map((item, index) => {
           return (
             <li key={index}>
-              <Circle letter={item} index={index} />
+              <Circle
+                letter={item}
+                index={index}
+                state={
+                  index === currIndex
+                    ? ElementStates.Changing
+                    : ElementStates.Default
+                }
+                tail={isTail(index) ? TAIL : ''}
+                head={isHead(index) ? HEAD : ''}
+              />
             </li>
           )
         })}
