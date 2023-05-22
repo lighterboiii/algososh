f1d9afd5ffc80a8d2d07cf305baa0130c88fd428
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2023-05-22 21:12:35 +0300

    написать тест для компонента Button

---
+++ src/components/ui/button/test/button.test.js
@@ -0,0 +1,29 @@
+import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Button } from '../button';
+
+describe('Тест компонента Button', () => {
+  it('Компонент Button с текстом отрисован корректно', () => {
+    const button = renderer.create(<Button text='Новый массив' />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it('Компонент Button без текста отрисован корректно', () => {
+    const button = renderer.create(<Button text='' />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it('Компонент Button в заблокированном состоянии отрисован корректно', () => {
+    const button = renderer.create(<Button disabled />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it('Компонент Button в заблокированном состоянии отрисован корректно', () => {
+    const button = renderer.create(<Button isLoader />).toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it('Корректно выполняется коллбек при клике на компонент Button', () => {
+    window.alert = jest.fn();
+    render(<Button text='Коллбек' onClick={() => alert('Протестировано')} />);
+    const button = screen.getByText('Коллбек');
+    fireEvent.click(button);
+    expect(window.alert).toHaveBeenCalledWith('Протестировано');
+  })
+});
